8b3b28d46bb5480bb7bef6a6c47014e9d8af8f9c
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2023-02-10 01:14:12 +0600

    Finally to work awesome

--- configuration.nix
+++ configuration.nix
@@ -83,6 +83,7 @@
   services.xserver.windowManager = {
         awesome = {
           enable = true;
+          package = inputs.nixpkgs-f2k.packages.${pkgs.system}.awesome-git;
 
           luaModules = lib.attrValues {
             # inherit (pkgs)
